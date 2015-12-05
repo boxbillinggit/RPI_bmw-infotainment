e81f824eb1254a7c332de400c380f48453907432
Lars Gunnarsson <lars.gunnarsson@one-infiniteloop.com>
2015-12-05 17:16:49 +0100

    modifying cmake-config files adding git commit has in debian package description for traceability

--- gateway/CPackConfig.cmake
+++ gateway/CPackConfig.cmake
@@ -1,13 +1,5 @@
-# ref: https://cmake.org/Wiki/CMake:Packaging_With_CPack
-
-# https://nowardev.wordpress.com/2009/12/27/how-to-create-a-debian-package-for-script-and-for-project-that-use-cmake/
-
-
-# http://git.one-infiniteloop.com/larsa/bmw-infotainment/blob/6d8699d4282672d22021673cd60e61176cd1e29f/gateway/nbproject/Package-Release.bash#L106
-
-# DEBUG: list installed files in package: dpkg-deb --contents openbm-gateway-1.0.0-Linux.deb
-
-set(CPACK_GENERATOR "DEB")
+# fetch Git info (TODO: generate changelog instead)
+execute_process(COMMAND git show -s --format=%ci\ @%h OUTPUT_VARIABLE GIT_COMMIT)
 
 # Inherited from CMakeLists.txt
 set(CPACK_PACKAGE_NAME 		${PROJECT_NAME})
@@ -21,8 +13,12 @@ set(CPACK_PACKAGE_CONTACT 		"lars.gunnarsson@one-infiniteloop.com")
 set(CPACK_RESOURCE_FILE_LICENSE 	"${CMAKE_CURRENT_SOURCE_DIR}/COPYING.txt")
 set(CPACK_PACKAGE_DESCRIPTION_FILE 	"${CMAKE_CURRENT_SOURCE_DIR}/readme.md")
 set(CPACK_PACKAGE_DESCRIPTION 		"OpenBM Gateway")
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY 	"OpenBM Gateway makes a connection between IBus hardware connected to
-					 serialport and TCP/IP connected clients.")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY 	"OpenBM Gateway makes a connection between IBus hardware connected to serialport and TCP/IP connected clients. Commit: ${GIT_COMMIT}")
 
+# CPack utilities
+set(CPACK_GENERATOR "DEB")
+include(CPack)
 
+# DEBUG: list installed files in package: dpkg-deb --contents openbm-gateway-1.0.0-Linux.deb
+# ref: 	https://cmake.org/Wiki/CMake:Packaging_With_CPack
 
