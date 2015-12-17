90bf0e16e11362499984d30743366269a8272bac
zaba <zaba141@o2.pl>
2015-12-17 15:18:18 +0100

    Installation script installs the necessary libraries and starts installations Plugin

---
+++ plugin/plugin.service.bmw-infotainment/guicallback/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+echo "Install Dependices"
+
+apt-get install python-dev gcc g++ libboost-program-options-dev libboost-thread-dev libboost-date-time-dev -y
+
+echo "Configure Gateway"
+
+cmake .. -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
+
+make
+
+echo "Install Success You Must Copy Category plugin.service.bmw-infotainment to Addons Kodi."
