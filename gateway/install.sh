7c3dd48acd1b1c62fcefde6db6236073ce108a4d
zaba <zaba141@o2.pl>
2015-12-17 15:05:56 +0100

    Installation script installs the necessary libraries and starts installations Gateway

---
+++ gateway/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+echo "Install Dependices"
+
+apt-get install make cmake autoconf gcc g++ libboost-program-options-dev libboost-thread-dev libboost-date-time-dev -y
+
+echo "Configure Gateway"
+
+cmake .. -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
+
+make && make install
+
+echo "Install Success"
