7b1bd4c22976634bcdb1066df54a7659198966a9
Jarek <jarek@phalcon.ovh>
2016-03-27 13:11:16 +0200

    Install GPIO Controller

--- plugin/plugin.service.bmw-infotainment/guicallback/install.sh
+++ plugin/plugin.service.bmw-infotainment/guicallback/install.sh
@@ -13,4 +13,15 @@ cmake .. -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
 
 make
 
+cd ..
+
+echo "Install GPIO Controller"
+
+wget https://pypi.python.org/packages/source/R/RPi.GPIO/RPi.GPIO-0.6.2.tar.gz
+tar -xvf RPi.GPIO-0.6.2.tar.gz
+cd RPi.GPIO-0.6.2
+sudo python setup.py install
+cd ..
+sudo rm -rf RPi.GPIO-0.*
+
 echo "Install Success You Must Copy Category plugin.service.bmw-infotainment to Addons Kodi."
